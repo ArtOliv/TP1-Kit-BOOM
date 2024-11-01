5389211f2e000aae906bc6a313217ab5dcecc331
Arthur Carvalho <arthur@Ubuntu22.04>
2024-10-31 22:32:07 -0300

    Primeiro commit

---
+++ Configuracao kit/kit.c
@@ -0,0 +1,11 @@
+#include "kit.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Caixa *criaCaixa(int largura, int altura){
+    Caixa *caixa = (Caixa*) malloc (sizeof(Caixa));
+
+    if(caixa != NULL){
+        
+    }
+}
